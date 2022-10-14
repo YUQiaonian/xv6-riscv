ad5e6d7156da956a4de7f85d5471c12017eea0da
YUQIAONIAN <qiaonianyu@gamil.com>
2022-10-14 13:14:10 +0800

    update primes

--- user/primes.c
+++ user/primes.c
@@ -2,51 +2,64 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-// void primes(int* arr, int len){
-//     if(len == 0){
-//         return;
-//     }
-//     int p[2];
-//     pipe(p);
-//     if(fork() == 0){
-//         //child for feed data
-//         close(p[0]);
-//         for(int i = 0; i < len; i++){
-//             write(p[1], &arr[i], 4);
-//         }
-//         wait(0);
-//     }
-//     else{
-//         //parent to read and judge prime
-//         close(p[1]);
-//         int prime;
-//         read(p[0], &prime, 4);
-//         fprintf(1, "prime %d\n", prime);
-//         int n;
-//         int cnt = 0;
-//         while(read(p[0], &n, 4) == 4){
-//             //fprintf(1,"%d\n",n);
-//             if(n % prime != 0){
-//                 *(arr++) = n;
-//                 cnt++;
-//                 //fprintf(1, "cnt = %d\n", cnt);
-//             }
-//         }
-//         //fprintf(1, "end cnt = %d\n", cnt);
-//         primes(arr - cnt, cnt);
-//         return;
-//     }
-//     wait(0);
-//     return;
-// }
+void primes(int fd){
+	int prime;
+	if(read(fd, &prime, 4) != 0){
+		fprintf(1, "prime %d\n", prime);
+	}
+	else{
+		return;
+	}
 
+	int n = -1;
+	int p[2];
+	pipe(p);
+	//fprintf(1, "fd %d p[0] %d p[1] %d\n", fd, p[0], p[1]);
+	while(1){
+		if(read(fd, &n, 4) == 0){
+			break;
+		}
+		else{
+			if(n%prime != 0){
+				write(p[1], &n, 4);
+			}
+		}
+	}
+
+	if(n == -1){
+		close(fd);
+		close(p[0]);
+		close(p[1]);
+		return;
+	}
+
+	if(fork() == 0){
+		//child
+		close(fd);
+		close(p[1]);
+		primes(p[0]);
+		close(p[0]);
+	}
+	else{
+		//parent
+		close(fd);
+		close(p[0]);
+		close(p[1]);
+		wait(0);
+		return;
+	}
+}
 
 int main(int argc, char* argv[]){
-    int arr[34];
-    for(int i = 2; i <= 35 ; i++){
-        arr[i-2] = i;
-    }
-    
+	int p[2];
+	pipe(p);
+	for(int i = 2; i <= 35; i++){
+		write(p[1], &i, 4);
+	}
+
+	close(p[1]);
+	primes(p[0]);
+	close(p[0]);
     
     return 0;
 }
